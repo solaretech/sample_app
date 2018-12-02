0463d3c52016af2ccbe7e72a3ce2b25ec0d86ae7
角谷一樹 <kazukikakuya@kadoyakazukinoMacBook-puro.local>
2018-12-02 13:38:07 +0900

    First Commit of Rails

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9575c80431435351e55f8d44fe314018f38575b050e9cf4dc3f14881c06f5eb32aa6b2b8bff0af0fd21e1763a87fa10e3228074df0f7e6c06bb80de40714279a'
